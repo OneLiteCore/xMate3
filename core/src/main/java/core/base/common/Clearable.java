3814a941f7c81a9574b19a1f59cd63be924ed9fa
DrkCore <178456643@qq.com>
2016-08-02 00:52:39 +0800

    导入core module

---
+++ core/src/main/java/core/base/common/Clearable.java
@@ -0,0 +1,8 @@
+package core.base.common;
+
+public interface Clearable {
+
+    boolean isCleared();
+
+    void clear();
+}
---
+++ core/src/main/java/core/base/common/RefreshRate.java
@@ -0,0 +1,7 @@
+package core.base.common;
+
+public enum RefreshRate {
+	
+	ALWAYS, ONCE, NEVER
+	
+}
